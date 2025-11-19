45706ddd6506781dfbbb2b3f66135b7864df659c
mdrpsvu-design <mdr_psvu@mail.ru>
2025-11-20 00:15:03 +0300

    Delete art_shop directory

--- art_shop/models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-
-class Item(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
-    description: str
-    price: int
-    image_url: str  # Путь к картинке
